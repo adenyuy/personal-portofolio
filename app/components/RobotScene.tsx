9769a7e40144f4138c134b40e714dd56c7f85bba
adenyuy <marvinugraha@gmail.com>
2026-07-07 16:45:59 +0700

    fix: resolve typescript error on robot animation

--- app/components/RobotScene.tsx
+++ app/components/RobotScene.tsx
@@ -34,8 +34,7 @@ export default function RobotScene() {
         <Suspense fallback={null}>
           <PresentationControls
             global
-            config={{ mass: 2, tension: 500 }}
-            snap={{ mass: 4, tension: 1500 }}
+            snap={true}
             rotation={[0, 0, 0]}
             polar={[-Math.PI / 4, Math.PI / 4]}
             azimuth={[-Math.PI / 4, Math.PI / 4]}
